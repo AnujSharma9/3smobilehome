af59158879f80c4bb5929b00fa0d5523969b7465
Anuj <anujbharadwaj101010@gmail.com>
2024-11-08 21:55:01 +0530

    2day

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from "react"
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
- 
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { signInUser } from "../Service/User";
 
 export function Signin(){
 
     const [showPassword , setShowPassword] = useState(false);
+    const [email, setEmail] = useState('')
+    const [password , setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const onSignin = async () => {
+        try {
+            if (email.length === 0) {
+                toast.warn('Please enter email');
+            } else if (password.length === 0) {
+                toast.warn('Please enter password');
+            } else {
+                const result = await signInUser(email, password);
+                if (result.status === 'Success') {
+                    toast.success('Welcome to the stock');
+                    navigate('/stock');
+                } else {
+                    toast.error(result['error']);
+                }
+            }
+        } catch (error) {
+            toast.error('Failed to connect to the backend');
+        }
+    };
+    
 
     return(
         <>
@@ -16,11 +40,12 @@ export function Signin(){
                 <div className="col">  <div className="form">
                  <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input onChange={(e)=>setEmail(e.target.value)}
+                    type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                   </div>
                   <label for="exampleInputPassword1" class="form-label">Password</label>
                    <div className="input-group mb-3">
-                            <input
+                            <input onChange={(e)=>setPassword(e.target.value)}
                                 type={showPassword ? "text" : "password"}
                                 className="form-control"
                                 placeholder="Password"
@@ -37,7 +62,7 @@ export function Signin(){
                         </div>
 
                 
-                   <button type="submit" class="btn btn-primary">Submit</button>
+                   <button onClick={onSignin} type="submit" class="btn btn-primary">Submit</button>
 
                    <div>Don't have an account ? <Link to={'/signup'}>Sigin-up here</Link> </div>
                 </div>
--- src/Service/User.js
+++ src/Service/User.js
@@ -3,10 +3,27 @@ import {createUrl,createError} from "./Utils"
 
 export async function signUpUser(name,phoneNumber,email,password) {
     try{
-        const url = createUrl('user/signip')
+        const url = createUrl('user/signup')
         const body = {
             name,
             phoneNumber,
+            email,
+            password
+        }
+        const response = await axios.post (url,body) 
+        
+        return response.data
+
+    }catch(ex){
+        return createError (ex)
+    }
+}
+
+export async function signInUser(email,password) {
+    try{
+        const url = createUrl('user/signip')
+        const body = {
+          
             email,
             password
         }
